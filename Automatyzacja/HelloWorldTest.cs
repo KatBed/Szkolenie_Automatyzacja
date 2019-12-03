ed13e1141db7bc4689e7d47125b6b32e4f64ea2f
KB <bjakbednarek@gmail.com>
2019-12-03 09:59:33 +0100

    dodajemy git gnore

---
+++ Automatyzacja/HelloWorldTest.cs
@@ -0,0 +1,23 @@
+﻿using Xunit; //pojawia się przy instalacji xunit
+
+namespace Automatyzacja
+{
+    public class HelloWorldTest //klasa musi byc publiczna
+    {
+        [Fact] //atrybut,alt+enter (using xunit)
+        public void CanSayHello() //nic nie zwraca, nic nie przyjmuje na wejściu
+        {
+            //arrange stan początkowy
+            var a = 1;
+            var b = 2;
+            //act test
+            var result = Function(a, b);
+            //assert czy t co dostalismy jest tym co chcieliśmy
+            Assert.Equal(42, result);
+        }
+        private int Function(int a, int b)
+        {
+            return 42;
+        }
+    }
+}
