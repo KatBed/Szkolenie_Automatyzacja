ab35048bb55ba79823a3ab9556914c97c6d337eb
KB <bjakbednarek@gmail.com>
2019-12-03 14:54:30 +0100

    zmiany w plikach, po obiedzie, dodanie komentarza do strony nową metodą

--- Automatyzacja/GoogleTests.cs
+++ Automatyzacja/GoogleTests.cs
@@ -1,5 +1,6 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Interactions;
 using System;
 using Xunit;
 
@@ -14,33 +15,44 @@ namespace Automatyzacja
         }
         [Fact]
 
-        public void CanGoogleWeatherForWarsaw()
+        public void CanAddNewComment()
         {
             browser.Navigate().GoToUrl("http://automatyzacja.benedykt.net/");
 
-            var queryBox = browser.FindElement(By.PartialLinkText("ghfjhjhvjhvjh"));
-            queryBox.Click();
-            var queryBox1 = browser.FindElement(By.Id("comment"));
-            queryBox1.Click();
-            queryBox1.SendKeys("komentarz dodany przez automat");
-            var queryBox2 = browser.FindElement(By.Id("author"));
-            queryBox2.SendKeys("Kasia Kasia");
-            var queryBox3 = browser.FindElement(By.Id("email"));
-            queryBox3.SendKeys("Kasia@Kasia.wp.pl");
-            var queryBox4 = browser.FindElement(By.Id("submit"));
-            queryBox4.Click();
+            var latestNote = browser.FindElement(By.CssSelector(".entry-title > a")); //klika w linka i przenosi do nowej strony
+            latestNote.Click();
 
+            //var firstNote = browseByElement(By.CssSelector("..entry-title"));
+            //var firstNoteUrl = firstNote.FindElement(By.TagName("a"));
+
+            var comment = browser.FindElement(By.Id("comment"));
+            var exampleText = Faker.Lorem.Paragraph();
+            comment.SendKeys(exampleText);
+
+            var author = browser.FindElement(By.Id("author"));
+            var exampleAuthor = Faker.Name.FullName();  //zmienna sxampleauthor
+            author.SendKeys(exampleAuthor);
+
+            var email = browser.FindElement(By.Id("email"));
+            var exampleEmail = Faker.Internet.Email();
+            email.SendKeys(exampleEmail);
+
+            MoveToElement(browser.FindElement(By.CssSelector("div.nav-links")));
+            browser.FindElement(By.Id("submit")).Submit();
+
+            var comments = browser.FindElements(By.CssSelector("article.comment-body"));
+            comments.Where(comments->)
+}
             //poniższa linijka służy do przesuwania elementu kiedy nie widać go na stronie
-            //private void MoveToElement(IWebElement element)
-            //{
-             //   Actions builder = new Actions (browser)
-             //Actions moveTo = builder.MoveToElement(element);
-             //moveTo.Build().Perform();
-            //}
+            private void MoveToElement(IWebElement element)
+            {
+                Actions builder = new Actions(browser);
+                Actions moveTo = builder.MoveToElement(element);
+                moveTo.Build().Perform();
+            }
 
-            Assert.Equal("komentarz dodany przez automat", result.Text);
-            
-        }
+            //Assert.Equal("komentarz dodany przez automat", result.Text);
+           
 
         public void Dispose() //metoda Dispose która robi quit
         {
